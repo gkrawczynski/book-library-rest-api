b1398d4766f04be38fe58636156324fc87f716dc
Grzegorz Krawczynski <grzegorzkrawczynski@yahoo.com>
2018-10-11 21:11:44 +0200

    modifications

--- src/main/java/pl/coderslab/config/AppConfig.java
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -28,6 +29,12 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     }
     // -- </ViewResolver> --
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 }
 
 
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -5,7 +5,17 @@
 </head>
 <body>
 
-<h1>Welcome to Books Library!</h1>
+<form id="form">
+    <input type="text", id="isbn" placeholder="ISBN"/>
+    <input type="text", id="title" placeholder="Title"/>
+    <input type="text", id="author" placeholder="Author"/>
+    <input type="text", id="publisher" placeholder="Publisher"/>
+    <input type="text", id="type" placeholder="Type"/>
+    <button id="button">Add book</button>
+</form>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="/resources/app.js", type="text/javascript"></script>
 
 </body>
 </html>
